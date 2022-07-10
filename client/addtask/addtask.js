2d22c1deea20fb7d6918740b94337f05dffe2ed1
KiaraHolloway <kiaraholloway12@gmail.com>
2022-07-10 08:50:45 -0400

    8:50

--- client/addtask/addtask.js
+++ client/addtask/addtask.js
@@ -5,6 +5,29 @@ Template.addtask.events ({
         "task": newtask
       })
         $("#newtask").val("");
+    },
+
+
+
+    'click .privateTask'() {
+        if ($(".fa-xmark").hasClass("d-none")) {
+            $(".fa-check").addClass("d-none");
+            $(".fa-xmark").removeClass("d-none");
+        }
+        else {
+            $(".fa-check").removeClass("d-none");
+            $(".fa-xmark").addClass("d-none");
+        }
+    },
+
+    'click .js-trash'() {
+        let taskId = this._id;
+       tododb.update ({_id: listId},{
+        set: {
+            "trashBin" : true
+        }
+    
+    });
     }
 
 });
--- client/main.js
+++ client/main.js
@@ -14,6 +14,7 @@ import '../lib/collection.js';
 
 
 
+
 // Template.hello.onCreated(function helloOnCreated() {
 //   // counter starts at 0
 //   this.counter = new ReactiveVar(0);
