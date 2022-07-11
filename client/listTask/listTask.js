24b4e72d0b97971f4186b7bd808c87c3645e93a5
KiaraHolloway <kiaraholloway12@gmail.com>
2022-07-11 10:07:09 -0400

    10:07

--- client/listTask/listTask.js
+++ client/listTask/listTask.js
@@ -3,3 +3,15 @@ Template.listTasks.helpers({
         return tododb.find();
     }
 });
+
+Template.listTasks.events({
+        'click .js-trashthis'(){
+            let taskID = this._id;
+            tododb.update({ _id:taskID},{
+                $set:{
+                    "trashBin":true
+                }
+
+            })
+        }
+});
