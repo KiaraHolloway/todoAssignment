dbb958c574f69d779f00ef99a0a8cdd8ef5d1547
kiaraHolloway <kiaraholloway12@gmail.com>
2022-07-13 12:26:58 -0400

    july13th

--- client/listTask/listTask.js
+++ client/listTask/listTask.js
@@ -13,5 +13,28 @@ Template.listTasks.events({
                 }
 
             })
+        },
+
+
+        'click .js-tick' (){
+            let taskID = this._id;
+            if (this.comp){
+                tododb.update({ _id:taskID},{
+                    $set:{
+                        comp:false
+                    }
+            });
+
+        } else{
+            tododb.update({ _id:taskID},{
+                $set:{
+                    comp:true
+                }
+        });
+            
         }
+
+    }
+
 });
+
